d3e6108d6d14747b68ab917d6e8f0e92785dd661
Kagami <shimura.kyohei7617@gmail.com>
2023-12-19 20:29:16 +0900

    feat: message log

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from './member';
+import { MessageService } from './message.service';
 import { MEMBERS } from './mock-members';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   // RxJs
   // Observable: 非同期処理
   getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: Get Employees.');
     return of(MEMBERS);
   }
 
